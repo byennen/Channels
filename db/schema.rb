a87dd9501ab361d05ac42c865020afa109d15a05
Lance Ennen <byennen@gmail.com>
2012-01-31 13:30:58 -0600

    roles added

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120131061807) do
+ActiveRecord::Schema.define(:version => 20120131175225) do
 
   create_table "channels", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "roles", :force => true do |t|
+    t.string   "name"
+    t.float    "price"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
