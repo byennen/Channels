10ea59904cbac282c9de7f703cbd4da6eee3d19a
Lance Ennen <byennen@gmail.com>
2012-02-20 00:43:49 -0700

    slideshow added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Channels::Application.routes.draw do
 
+  resources :channels
+
   root :to => "home#index"
 
+  devise_for :users
+  resources :users, :only => :show
+
+  #admin
+  namespace :admin do
+    root :to => "dashboard#index"
+    get "dashboard/help"
+    resources :channels
+  end
 end
