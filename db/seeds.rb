d15a6ad15d3db8e52c98b680d41e738c065e6ca1
Zeljko Dakic <zeljko@dakic.com>
2012-04-17 11:35:53 -0500

    zd adding songs and updating seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,13 @@
 puts 'SETTING UP DEFAULT USER LOGIN'
 user = User.create! :first_name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user.first_name
+puts ""
+puts "Creating Channels"
+channel = Channel.create(:name => "Randy Owen", :subdomain => "randy-owen")
+Channel.create(:name => "Mark Collie", :subdomain => "mark-collie")
+Channel.create(:name => "Sam Phillips", :subdomain => "sam-phillips")
+puts "Creating songs"
+Song.create(:channel => channel, :title => "Breaking Bad", :filename => "song.mp3", :active => true)
+Song.create(:channel => channel, :title => "D.V.N.O.", :filename => "dvno.mp3", :active => true)
+Song.create(:channel => channel, :title => "Genesis", :filename => "genesis.mp3", :active => true)
+Song.create(:channel => channel, :title => "Threee Paaarrrtyyy", :filename => "party.mp3", :active => true)
