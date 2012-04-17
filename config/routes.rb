87d73132bbf0f9d8b5257d3eca0a9eeb15bbb6cb
Lance Ennen <byennen@gmail.com>
2012-04-17 03:15:23 -0500

    hiding routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Channels::Application.routes.draw do
 
   #static pages
   match "/about" => "pages#about"
-  match "/privacy" => "pages#privacy"
-  match "/refund" => "pages#refund"
+  # match "/privacy" => "pages#privacy"
+  # match "/refund" => "pages#refund"
   match "/randy-owen-altimarc-coming-soon-video" => "pages#randy_owen_coming_soon_video", :as => "randy_owen_video"
 
   #giveaways
@@ -19,30 +19,27 @@ Channels::Application.routes.draw do
   match "/create_derby(/:id)" => "giveaways#create_derby"
   match "/create_cma(/:id)" => "giveaways#create_cma"
 
-  #channels
-  resources :channels, :only => :show
-
   #users
-  devise_for :users, :controllers => {:registrations => 'registrations', :sessions => 'sessions'}
-  devise_scope :user do
-    get "/login" => "sessions#new"
-    delete '/logout' => 'sessions#destroy'
-    get '/logout' => 'sessions#destroy'
-    get '/signup' => 'registrations#new'
-  end
+  # devise_for :users, :controllers => {:registrations => 'registrations', :sessions => 'sessions'}
+  # devise_scope :user do
+  #   get "/login" => "sessions#new"
+  #   delete '/logout' => 'sessions#destroy'
+  #   get '/logout' => 'sessions#destroy'
+  #   get '/signup' => 'registrations#new'
+  # end
 
   #admin
-  namespace :admin do
-    root :to => "dashboard#index"
-    get "dashboard/help"
-    resources :channels do
-      resources :songs
-      resources :features, :only => :edit
-    end
-    resources :features, :only => :update
-    resources :songs
-    resources :users, :only => :index
-  end
+  # namespace :admin do
+  #   root :to => "dashboard#index"
+  #   get "dashboard/help"
+  #   resources :channels do
+  #     resources :songs
+  #     resources :features, :only => :edit
+  #   end
+  #   resources :features, :only => :update
+  #   resources :songs
+  #   resources :users, :only => :index
+  # end
 
   root :to => "home#index"
 
