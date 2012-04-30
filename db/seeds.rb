68e6243e428892d27832e1e2f8ad3a1b7ed063dc
Lance Ennen <byennen@gmail.com>
2012-04-30 09:58:50 -0700

    Lance: seed file update for channel master

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,32 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :first_name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please', :role => 'admin'
-puts 'New user created: ' << user.first_name
+
+#Delete Users
+User.delete_all
+
+puts "***SETTING UP ADMIN USER LOGIN***"
+user = User.create! :first_name => "Lance", :last_name => "Ennen", :email => "admin@altimarc.com", :password => "please", :password_confirmation => "please", :role => "admin"
+puts "New user created: " << user.email
+puts "Password: " << user.password
 puts ""
-puts "Creating Channels"
+
+
+puts "***CREATING CHANNELS***"
 channel = Channel.create(:name => "Randy Owen", :subdomain => "randy-owen")
 Channel.create(:name => "Mark Collie", :subdomain => "mark-collie")
 Channel.create(:name => "Sam Phillips", :subdomain => "sam-phillips")
-puts "Creating songs"
+puts ""
+
+
+puts "***SETTING UP CHANNEL MASTER USER LOGIN***"
+user = User.create! :first_name => "Alison", :last_name => "Owen", :email => "channel@altimarc.com", :password => "please", :password_confirmation => "please", :role => "channel_master", :channel_id => "1"
+puts "New channel master user created: " << user.email
+puts "Password: " << user.password
+puts ""
+
+
+puts "***CREATING SONGS***"
 Song.create(:channel => channel, :title => "Breaking Bad", :filename => "song.mp3", :active => true)
 Song.create(:channel => channel, :title => "D.V.N.O.", :filename => "dvno.mp3", :active => true)
 Song.create(:channel => channel, :title => "Genesis", :filename => "genesis.mp3", :active => true)
 Song.create(:channel => channel, :title => "Threee Paaarrrtyyy", :filename => "party.mp3", :active => true)
+puts ""
