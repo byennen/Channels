094d54150570b29c36d01c2f605c07e83c7abee6
Lance Ennen <byennen@gmail.com>
2012-05-02 10:53:00 -0700

    adding login for staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  #password for staging
+  if (ENV["RAILS_ENV"] == "staging")
+   before_filter :verifies_staging_user
+  end
+
   def after_sign_in_path_for(resource)
     stored_location_for(resource) ||
       if resource.is_a?(User)
@@ -29,4 +34,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+
+  def verifies_staging_user
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "altimarc" && password == "altimarc2012"
+    end
+  end
+
 end
