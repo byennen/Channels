156f9ce7b64ff913b3e3449aa25f146f9c127638
Nate Jackson <nate.d.jackson@gmail.com>
2012-05-08 13:24:35 -0500

    changed stripe env variable

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
 stripe_config = YAML.load(File.open(File.join(Rails.root, 'config', 'stripe.yml')))
 
-Stripe.api_key =    ENV['API_KEY']           || stripe_config[:api_key]
+Stripe.api_key =    ENV['STRIPE_API_KEY']           || stripe_config[:api_key]
 STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY'] || stripe_config[:public_key] 
