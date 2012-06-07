5e2f44eed9f4475ce0eed4879fe477159b793225
Zeljko Dakic <zeljko@dakic.com>
2012-06-06 23:14:58 -0500

    zd fixing seed file, now each song has upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,17 +26,10 @@ album = Album.create(:channel => channel, :title => "Demo", :description => "Dem
 puts ""
 
 puts "***CREATING SONGS***"
-song = Song.create(:album => album, :title => "Breaking Bad", :active => true, :price => "1.00")
-Song.create(:album => album, :title => "D.V.N.O.", :active => true, :price => "1.00")
-Song.create(:album => album, :title => "Genesis", :active => true, :price => "1.00")
-Song.create(:album => album, :title => "Threee Paaarrrtyyy", :active => true, :price => "1.00")
-puts ""
-
-puts "***CREATING SONG UPLOADS***"
-song.uploads.create(:file_name => "song.mp3")
-song.uploads.create(:file_name => "dvno.mp3")
-song.uploads.create(:file_name => "genesis.mp3")
-song.uploads.create(:file_name => "party.mp3")
+Song.create(:album => album, :title => "Breaking Bad", :active => true, :price => "1.00").uploads.create(:file_name => "song.mp3")
+Song.create(:album => album, :title => "D.V.N.O.", :active => true, :price => "1.00").uploads.create(:file_name => "dvno.mp3")
+Song.create(:album => album, :title => "Genesis", :active => true, :price => "1.00").uploads.create(:file_name => "genesis.mp3")
+Song.create(:album => album, :title => "Threee Paaarrrtyyy", :active => true, :price => "1.00").uploads.create(:file_name => "party.mp3")
 puts ""
 
 puts "***CREATING VIDEOS***"
