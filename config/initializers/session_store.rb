5e1a5a4f7839c71ca3f56dbc6885fd6bec57364c
Jim Ray <jimiray@gmail.com>
2012-06-13 22:23:44 -0600

    add domains for cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
-Channels::Application.config.session_store :cookie_store, key: '_channels_session'
+def domain
+  if Rails.env.production?
+    ".altimarc.com"
+  elsif Rails.env.staging?
+    ".altimarc-staging.com"
+  elsif Rails.env.development?
+    ".channels.dev"
+  end
+end
+Channels::Application.config.session_store :cookie_store, {key: '_channels_session',
+                                                           domain: domain}
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
