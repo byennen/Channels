f1af0425f779c0724651d58eed85fdebb76756f1
Zeljko Dakic <zeljko@dakic.com>
2012-06-15 12:17:07 -0500

    zd removing wrongly named file

--- app/models/ads.rb
+++
@@ -1,7 +0,0 @@
-class Ads < ActiveRecord::Base
-  attr_accessible :channel_id, :published, :ad_file
-  has_many :uploads, as: :uploadable
-  #belongs_to :uploadable, polymorphic: true
-  mount_uploader :ad_file, SongUploader
-
-end
