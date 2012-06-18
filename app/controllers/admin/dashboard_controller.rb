e4776643c8fdcf0286305f7366b740262559ac1f
Lance Ennen <byennen@gmail.com>
2012-06-18 13:19:15 -0500

    uncomment code from the other night - adding links in side nav

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,12 +1,12 @@
 class Admin::DashboardController < Admin::ApplicationController
-  #load_and_authorize_resource :channel
+  load_and_authorize_resource :channel
 
   def index
-    # if current_user.channel_master?
-    #   redirect_to admin_channel_path(current_user.channel)
-    # else
-    #   authorize_admin
-    # end
+    if current_user.channel_master?
+      redirect_to admin_channel_path(current_user.channel)
+    else
+      authorize_admin
+    end
   end
 
   def channel_help
