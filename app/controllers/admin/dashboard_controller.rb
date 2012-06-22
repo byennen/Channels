7fe98dcc104771a1d6c16dbfbe742b623baa0270
Lance Ennen <byennen@gmail.com>
2012-06-22 11:43:40 -0600

    fixing dashboard for channel masters

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,11 +2,12 @@ class Admin::DashboardController < Admin::ApplicationController
   load_and_authorize_resource :channel
 
   def index
-    if current_user.channel_master?
-      redirect_to admin_channel_path(current_user.channel)
-    else
-      authorize_admin
-    end
+    @channel = current_user.channel
+    # if current_user.channel_master?
+    #   redirect_to admin_channel_path(current_user.channel)
+    # else
+    #   authorize_admin
+    # end
   end
 
   def channel_help
