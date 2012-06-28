51e210e1c086d7ca42f479dffccab5c79e8e8892
Jim Ray <jimiray@gmail.com>
2012-06-27 20:57:54 -0600

    add video link to show

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,14 +1,15 @@
 class VideosController < ApplicationController
   # load_and_authorize_resource :channel
   # load_and_authorize_resource :video, :through => :channel
-  
+
   respond_to :html
-  
+
   def index
     @channels = Channel.all
     @videos = Video.all
   end
-  
+
   def show
+    @video = Video.find(params[:id])
   end
 end
