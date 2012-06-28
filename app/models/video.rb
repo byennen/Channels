36d1f866472286d252970771579a0c2b70d69bd8
Jim Ray <jimiray@gmail.com>
2012-06-28 15:08:05 -0600

    add videos to show

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,17 +1,32 @@
 class Video < ActiveRecord::Base
+
+  VIDEO_EXTENSIONS = ['mp4', 'ogg', 'webm']
+
   include Rails.application.routes.url_helpers
-  
+
   belongs_to :channel
-  
+
   attr_accessible :id, :channel_id, :title, :description, :image, :paid, :active, :channel, :video, :preview
   image_accessor :image
-  
+
   mount_uploader :video, VideoUploader
   mount_uploader :preview, VideoUploader
-  
+
   scope :published, where('active IS NOT NULL AND active = 1')
   scope :recent, published.order(:created_at).limit(8)
-  
+
+  def filename
+    read_attribute :video
+  end
+
+  def sources
+    sources = []
+    file_extension = filename.split('.').last
+    VIDEO_EXTENSIONS.each do |ext|
+      sources << video.url.gsub(file_extension, ext)
+    end
+    sources
+  end
   #one convenient method to pass jq_upload the necessary information
   def to_jq_upload
     {
@@ -19,8 +34,8 @@ class Video < ActiveRecord::Base
       "size" => video.size,
       "url" => video.url,
       "delete_url" => admin_channel_video_path(channel.id, id),
-      "delete_type" => "DELETE" 
+      "delete_type" => "DELETE"
     }
   end
-  
+
 end
--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,8 +1,8 @@
 class VideoUploader < CarrierWave::Uploader::Base
   include Rails.application.routes.url_helpers
-  
+
   after :store, :zencode
-  
+
   def store_dir
     "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -11,19 +11,15 @@ class VideoUploader < CarrierWave::Uploader::Base
     %w(mov avi mp4 mkv wmv mpg)
   end
 
-  # def filename
-  #  "video.mp4" if original_filename
-  # end
-  
   def file_prefix
     original_filename.split('.').first
   end
-  
+
   def host
     Rails.env.production? ? 'www.altimarc.com' : 'www.altimarc-staging.com'
   end
 
-  private 
+  private
 
   def zencode(args)
     Rails.logger.debug("fog directory is #{fog_directory}")
@@ -34,21 +30,21 @@ class VideoUploader < CarrierWave::Uploader::Base
                                                            :notifications => [zencoder_callback_url(:protocol => 'http', :host => host)],
                                                            :quality => 3,
                                                            :format => "mp4",
-                                                           :public => 1},
+                                                           :public => true},
                                                           {:base_url => "s3://#{fog_directory}/#{store_dir}",
                                                            :filename => "#{file_prefix}.ogg",
                                                            :label => "ogg",
                                                            :notifications => [zencoder_callback_url(:protocol => 'http', :host => host)],
                                                            :quality => 3,
                                                            :format => "ogg",
-                                                           :public => 1},
+                                                           :public => true},
                                                           {:base_url => "s3://#{fog_directory}/#{store_dir}",
                                                            :filename => "#{file_prefix}.webm",
                                                            :label => "webm",
                                                            :notifications => [zencoder_callback_url(:protocol => 'http', :host => host)],
                                                            :quality => 3,
                                                            :format => "webm",
-                                                           :public => 1}]
+                                                           :public => true}]
                                               })
 
     Rails.logger.debug("Zencoder response is #{zencoder_response.inspect}")
