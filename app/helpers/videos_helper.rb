43735e98d439ffe0de42eba961facb530a7cd9d6
Jim Ray <jimiray@gmail.com>
2012-07-02 15:23:08 -0600

    fix json

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -3,7 +3,7 @@ module VideosHelper
   def video_playlist(sources)
     s = []
     sources.each_with_index do |source, index|
-      s << {index => {src: source[:src], type: source[:type]}}.to_json
+      s << "#{index}: {src: '#{source[:src]}', type: '#{source[:type]}'}"
     end
     return s.join(',')
   end
