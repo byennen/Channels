ccade948ab45b98d2f91eb88ae0d4b89c92567e9
Jim Ray <jimiray@gmail.com>
2012-07-09 09:56:28 -0600

    fixes for cancan loading error on song index

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -2,12 +2,14 @@ class Admin::SongsController < Admin::ApplicationController
   load_and_authorize_resource :channel
   load_and_authorize_resource :album
   # this is breaking channel masters song uploads
-  load_and_authorize_resource :song, :through => :album
-  
+  load_and_authorize_resource :song, :through => :album, :new => :index
 
   respond_to :html, :json
 
-  def index; end
+  def index
+    @songs = @album.songs
+    logger.debug("Songs are #{@songs.inspect}")
+  end
 
   def edit; end
 
@@ -16,16 +18,16 @@ class Admin::SongsController < Admin::ApplicationController
     @song.title = params[:song][:song].original_filename
     if @song.save
       respond_to do |format|
-        format.html {  
-          render :json => [@song.to_jq_upload].to_json, 
+        format.html {
+          render :json => [@song.to_jq_upload].to_json,
           :content_type => 'text/html',
           :layout => false
         }
-        format.json {  
-          render :json => [@song.to_jq_upload].to_json			
+        format.json {
+          render :json => [@song.to_jq_upload].to_json
         }
       end
-    else 
+    else
       render :json => [{:error => "custom_failure"}], :status => 304
     end
   end
@@ -43,4 +45,3 @@ class Admin::SongsController < Admin::ApplicationController
   end
 
 end
-  
