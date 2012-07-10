0048592916f464a343b109fc4eef11f9831c0a28
Jim Ray <jimiray@gmail.com>
2012-07-10 14:17:37 -0600

    add resque restart to cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 require 'capistrano/ext/multistage'
 require "bundler/capistrano"
 #require 'airbrake/capistrano'
+require 'capistrano-resque'
 
 set :application, "Channels"
 set :user, "deploy"
@@ -18,6 +19,9 @@ set :repository, "git@github.com:altimarc/Channels.git"
 set :deploy_via, :remote_cache
 ssh_options[:forward_agent] = true
 
+# resque workers
+set :workers, { "waveform_queue" => 1 }
+
 # automatically called after a deploy
 deploy.task :restart, :roles => :app do
   run "touch #{current_release}/tmp/restart.txt"
@@ -33,5 +37,7 @@ end
 
 after "deploy:update_code", "customs:config"
 after "deploy", "deploy:cleanup"
-        require './config/boot'
-        require 'airbrake/capistrano'
+after "deploy:restart", "resque:restart"
+
+require './config/boot'
+require 'airbrake/capistrano'
