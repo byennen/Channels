2cac9b5c3f841dfd15886d4dab18265624cf7852
Jim Ray <jimiray@gmail.com>
2012-07-10 15:15:51 -0600

    temp fix for resque, need to figure out how to get prod running now

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,3 +2,5 @@ role :app, "107.21.119.78"                          # Your HTTP server, Apache/e
 role :db, "107.21.119.78", :primary => true
 set :branch, :staging
 set :rails_env, "staging"
+
+after "deploy:restart", "resque:restart"
