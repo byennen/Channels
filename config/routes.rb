6a91a2fa7c17302f10eefb6f071caea42b46da96
Lance Ennen <byennen@gmail.com>
2012-07-18 00:33:44 -0500

    job page and other updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Channels::Application.routes.draw do
   #static pages
   match "about" => "pages#about"
   match "jobs" => "pages#jobs"
-  match "our_team" => "pages#our_team", :as => "our_team"
+  match "our-team" => "pages#our_team", :as => "our_team"
   match "press" => "pages#press"
   match "faq" => "pages#faq"
   match "customer-support" => "pages#customer_support", :as => "customer_support"
