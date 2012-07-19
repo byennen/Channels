4a7effc1e03f4e3d81df09984248360182d41dd4
Jim Ray <jimiray@gmail.com>
2012-07-19 16:51:41 -0600

    add bypass true to skip warden callbacks for FB issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create_password
     current_user.attributes = params[:user]
     if current_user.save
+      sign_in(current_user, :bypass => true)
       current_user.update_attribute(:new_fb_user, false)
       redirect_to root_url
     else
