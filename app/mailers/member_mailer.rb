59104d205253a5ff9bbed7f49b273a18ff3e1e62
Jim Ray <jimiray@gmail.com>
2012-07-20 13:31:55 -0600

    add mailers

---
+++ app/mailers/member_mailer.rb
@@ -0,0 +1,12 @@
+class MemberMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  def welcome(user_id)
+    @user = User.find(user_id)
+    @email = @user.email
+    @subject = "Welcome to Altimarc"
+    headers['X-Campaign-Id'] = "UserSignup"
+    mail(to: @email, subject: @subject)
+  end
+
+end
