0e26b79d9c4cc22352f35bd7ddaed30631426bae
Jim Ray <jimiray@gmail.com>
2012-07-23 08:24:13 -0600

    remove pinterest

--- db/migrate/20120723141243_add_social_networks_to_channels.rb
+++ db/migrate/20120723141243_add_social_networks_to_channels.rb
@@ -1,7 +1,6 @@
 class AddSocialNetworksToChannels < ActiveRecord::Migration
   def change
     add_column :channels, :facebook_page, :string
-    add_column :channels, :pinterest, :string
     add_column :channels, :twitter_id, :string
     add_column :channels, :google_plus_page, :string
   end
