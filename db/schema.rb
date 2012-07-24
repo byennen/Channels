38e86b7b1b4c5c8fb74842ece67333e04633f51c
Lance Ennen <byennen@gmail.com>
2012-07-24 17:40:45 -0500

    moving around layout

--- db/schema.rb
+++ db/schema.rb
@@ -185,16 +185,6 @@ ActiveRecord::Schema.define(:version => 20120724210651) do
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
-  create_table "video_encodings", :force => true do |t|
-    t.integer  "video_id"
-    t.string   "type"
-    t.string   "text"
-    t.string   "filename"
-    t.string   "status"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "videos", :force => true do |t|
     t.integer  "channel_id"
     t.string   "title"
