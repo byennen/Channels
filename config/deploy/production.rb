4ccbc298645fc4092ae0b6fc4089b3d4a84d84db
Jim Ray <jimiray@gmail.com>
2012-07-26 11:48:45 -0600

    add production branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 role :app, "107.22.185.71"                          # Your HTTP server, Apache/etc
 role :db, "107.22.185.71", :primary => true
-set :branch, :master
+set :branch, :production
 set :rails_env, "production"
 set :deploy_to, "/mnt/apps/#{application}"
