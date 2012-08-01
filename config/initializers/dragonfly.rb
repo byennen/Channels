7138bbe095c6346fb37327259ebe56e0f249a66e
Jim Ray <jimiray@gmail.com>
2012-08-01 15:47:25 -0600

    refactor initializer to use APP_CONFIG

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,35 +1,25 @@
 require 'dragonfly'
 app = Dragonfly[:images]
-
 app.configure_with(:imagemagick)
 app.configure_with(:rails)
 if Rails.env.development?
   app.configure do |c|
     c.datastore = Dragonfly::DataStorage::S3DataStore.new(
-      :bucket_name => 'altimarc-dev',
-      :access_key_id => 'AKIAIPHZNJZ5QBOUOTHA',
-      :secret_access_key => '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'
+      :bucket_name => APP_CONFIG['aws_bucket'],
+      :access_key_id => APP_CONFIG['aws_access_key'],
+      :secret_access_key => APP_CONFIG['aws_secret_access_key']
     )
     c.convert_command = "/usr/local/bin/convert"          # defaults to "convert"
     c.identify_command = "/usr/local/bin/identify"        # defaults to "identify"
   end
 end
-if Rails.env.production?
-  app.configure do |c|
-    c.datastore = Dragonfly::DataStorage::S3DataStore.new(
-      :bucket_name => 'altimarc-production',
-      :access_key_id => 'AKIAIPHZNJZ5QBOUOTHA',
-      :secret_access_key => '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'
-    )
-  end
-end
 
-if Rails.env.staging?
+if Rails.env.production? || Rails.env.staging?
   app.configure do |c|
     c.datastore = Dragonfly::DataStorage::S3DataStore.new(
-      :bucket_name => 'altimarc-staging',
-      :access_key_id => 'AKIAIPHZNJZ5QBOUOTHA',
-      :secret_access_key => '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'
+      :bucket_name => APP_CONFIG['aws_bucket'],
+      :access_key_id => APP_CONFIG['aws_access_key'],
+      :secret_access_key => APP_CONFIG['aws_secret_access_key']
     )
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,19 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
-if Rails.env.development?
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, APP_CONFIG['facebook_app_id'], APP_CONFIG['facebook_app_secret'], scope: "email, publish_stream,publish_actions"
-  end
-end
 
-if Rails.env.staging?
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '344575558914076', '23a4c1e2d05fef2d4e40101d6c58dd35', scope: "email, publish_stream,publish_actions"
-  end
-end
-
-if Rails.env.production?
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '364007843633134', '09933ed43f8aabdb9e2bd6200f8e5006', scope: "email, publish_stream,publish_actions"
-  end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, APP_CONFIG['facebook_app_id'], APP_CONFIG['facebook_app_secret'], scope: "email, publish_stream,publish_actions"
 end
