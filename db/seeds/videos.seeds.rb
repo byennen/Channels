1ca25fbae6bc3baac105c2f96a86ac9fbbbc681e
Lance Ennen <byennen@gmail.com>
2012-08-09 10:36:22 -0500

    fixing trucating issue on videos

--- db/seeds/videos.seeds.rb
+++ db/seeds/videos.seeds.rb
@@ -7,7 +7,7 @@ after :users, :channels do
 
   (1..8).each do |i|
     Video.create(
-      :channel => channel, :title => "This is a movie demo", :image => video_img, :publish_on => 1.day.ago, 
+      :channel => channel, :title => "This is a movie demo making this title longer so I can test", :image => video_img, :publish_on => 1.day.ago, 
       :description => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit lacus eu risus porttitor eget aliquet nisl venenatis. Nulla lacus libero, molestie et vulputate id, ullamcorper sed nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis euismod malesuada turpis sodales commodo. Phasellus fermentum sapien nec lectus luctus a viverra arcu ultrices. Integer eleifend varius lorem, sit amet suscipit mi imperdiet sed. In hac habitasse platea dictumst. Suspendisse egestas, sem condimentum condimentum blandit, neque sapien adipiscing elit, sed lacinia enim lorem eget arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia."
     )
   end
