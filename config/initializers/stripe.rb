a0533ba629db4d11b2c78391c6116552130ccf93
Jim Ray <jimiray@gmail.com>
2012-08-10 17:19:44 -0600

    update stripe settings into settings.yml

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,2 @@
-stripe_config = YAML.load(File.open(File.join(Rails.root, 'config', 'stripe.yml')))
-
-Stripe.api_key =    ENV['STRIPE_API_KEY']           || stripe_config[:api_key]
-STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY'] || stripe_config[:public_key] 
+Stripe.api_key =    APP_CONFIG['stripe_api_key']
+STRIPE_PUBLIC_KEY = APP_CONFIG['stripe_public_key']
