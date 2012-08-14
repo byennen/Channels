73ee22c9775ce3d81832dccfc3908686f019d919
Jim Ray <jimiray@gmail.com>
2012-08-14 13:30:26 -0600

    add previews to video if not member or paid

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -1,5 +1,6 @@
 module VideosHelper
-  def video_playlist(sources)
+  def video_playlist(video)
+    sources = current_user && current_user.paid_member? ? video.sources : video.video_preview.sources
     s = []
     sources.each_with_index do |source, index|
       s << "#{index}: {src: '#{source[:src]}', type: '#{source[:type]}'}"
--- app/models/video_preview.rb
+++ app/models/video_preview.rb
@@ -6,6 +6,19 @@ class VideoPreview < ActiveRecord::Base
   
   mount_uploader :video, VideoUploader
 
+  def filename
+    read_attribute :video
+  end
+  
+  def sources
+    sources = []
+    file_extension = filename.split('.').last
+    Video::VIDEO_EXTENSIONS.each do |ext|
+      sources << {:src => video.url.gsub(file_extension, ext[:ext]), :type => ext[:type] }
+    end
+    sources
+  end
+  
   def processed!
     update_attribute(:processed, true)
   end
