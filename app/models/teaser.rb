9d1903eb1907925404682bcddaddc5040c83c433
Jim Ray <jimiray@gmail.com>
2012-08-14 14:15:34 -0600

    add teasers to video playlist

--- app/models/teaser.rb
+++ app/models/teaser.rb
@@ -9,4 +9,16 @@ class Teaser < ActiveRecord::Base
     update_attribute(:processed, true)
   end
     
+  def filename
+    read_attribute :video
+  end
+  
+  def sources
+    sources = []
+    file_extension = filename.split('.').last
+    Video::VIDEO_EXTENSIONS.each do |ext|
+      sources << {:src => video.url.gsub(file_extension, ext[:ext]), :type => ext[:type] }
+    end
+    sources
+  end
 end
