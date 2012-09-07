4d5d621ce299af0c04cb78694cb2fdc9256fcdbc
Lance Ennen <byennen@gmail.com>
2012-09-07 15:58:59 -0600

    testing staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,5 @@ role :db, "107.21.119.78", :primary => true
 role :resque_worker, "107.21.119.78", :primary => true
 set :branch, :staging
 set :rails_env, "staging"
-
+set :deploy_to, "/mnt/apps/#{application}"
 after "deploy:restart", "resque:restart"
