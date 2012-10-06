e727a5bd5749bae9a29b86a696cac966db4d148e
Jim Ray <jimiray@gmail.com>
2012-10-06 13:35:53 -0600

    redirect to channel home page or home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,11 @@ class SessionsController < Devise::SessionsController
     user = User.from_omniauth(env["omniauth.auth"])
     sign_in(User, user)
     session[:user_id] = user.id
-    if user.new_fb_user?
-      flash[:notice] = "Thank you for connecting via Facebook"      
-      render :action => :new
+    flash[:notice] = "Thank you for connecting via Facebook"      
+    if channel = Channel.find_by_uuid(params[:state])
+      redirect_to "http://#{channel.subdomain}.#{APP_CONFIG['domain']}"
     else
-      flash[:notice] = "Thank you for connecting via Facebook"       
-      if channel = Channel.find_by_uuid(params[:state])
-        redirect_to "http://#{channel.subdomain}.#{APP_CONFIG['domain']}"
-      else
-        redirect_to root_url
-      end
+      redirect_to root_url
     end
   end
 
