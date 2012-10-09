8ffbd86f75101b5e8c82468e53ab249211d94e9d
Jim Ray <jimiray@gmail.com>
2012-10-08 19:28:23 -0600

    fix join url for devise reset password

--- config/routes.rb
+++ config/routes.rb
@@ -81,6 +81,7 @@ Channels::Application.routes.draw do
     delete '/logout' => 'sessions#destroy'
     get '/logout' => 'sessions#destroy'
     get '/signup' => 'registrations#new'
+    get '/join' => 'registrations#new', :as => 'join'
     #match 'logout', to: 'sessions#destroy', as: 'logout'
 
     #facebook
