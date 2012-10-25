fd9f515c60af21788f65728cf81e59f8f057d518
Lance Ennen <byennen@gmail.com>
2012-10-25 15:38:06 -0500

    try add caching to home/index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  caches_page :index
+
   def index
     @featured_channel = Channel.find_by_subdomain('fatherofrocknroll')
     @featured_song = Song.recent.first
