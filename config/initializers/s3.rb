df9dbd00ae12f796446d3418b0674953aeb664b5
Lance Ennen <byennen@gmail.com>
2013-04-29 09:58:16 -0400

    update gems - remove old s3 and update key and secret - zencoder needs to be paid for if we want video uploads

--- config/initializers/s3.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env.development?
-  ENV['S3_UPLOADER_BUCKET'] = "altimarc-test"
-  ENV['S3_UPLOADER_ACCESS_KEY'] = "AKIAIPHZNJZ5QBOUOTHA"
-  ENV['S3_UPLOADER_SECRET_ACCESS_KEY'] = "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"
-end
